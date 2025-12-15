51b3d50e96d438af6eb1b73bbb05a83d9731a7d7
ujjwal-basnet <ujjwalvictus15@gmail.com>
2025-12-15 19:46:40 +0545

    basic

---
+++ write_message.py
@@ -0,0 +1,28 @@
+from email.encoders import encode_7or8bit
+from jinja2 import Environment , FileSystemLoader
+from pathlib import Path
+
+
+max_score= 100 
+test_name= "Python Challenge"
+students = [
+       {"name": "Sandrine",  "score": 100},
+    {"name": "Gergeley", "score": 87},
+    {"name": "Frieda", "score": 92},
+]
+
+environment= Environment(loader= FileSystemLoader("templates/"))
+template= environment.get_template("message.txt")
+
+for student in students: 
+    filename = Path(f"output/basic/message_{student['name'].lower()}.txt")
+    filename.parent.mkdir(parents=True, exist_ok=True)
+    content= template.render(
+        student,
+        max_score=max_score,
+        test_name=test_name
+    )
+
+    with open(filename , mode='w', encoding='utf-8')as message:
+        message.write(content)
+        print(f"wrote {filename}")
